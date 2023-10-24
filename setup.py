1cc567164cdb01e913f5bfd3c4c89615e0a17115
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-10-24 18:52:39 +0300

    getting good package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='prob-distributions',
+setup(name='dist',
       version='1.0.0',
       description='Provides functionality to work with Probability distributions and different sampling techniques',
       packages=['distributions','sampling-methods'],
