c9792b72000f6240e923514183a37506a5073c92
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-10-24 17:03:51 +0300

    updated setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob-distributions',
+      version='1.0.0',
+      description='Provides functionality to work with Probability distributions and different sampling techniques',
+      packages=['distributions','sampling-methods'],
+      zip_safe=False)
